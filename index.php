4a57f3e2e6f1c061bb767f52765faef7ee9c632d
63070052 <73658351+63070052@users.noreply.github.com>
2020-10-30 12:42:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
-<?php
-echo "<h2>IT@KMITL</h2>";
-echo "Hello IT<br>";
-?>
+<html>
+
+<head>
+    <title>Hello World</title>
+</head>
+
+<body>
+    <h1>Nutthida Plangkam</h1>
+    <p>POP</p>
+    <img src="01.jpg">
+    <img src="02.jpg">
+    <br>
+    <a href="https://www.instagram.com/cats_of_instagram/?hl=en">This is a link</a>
+
+    <h2>Jokes</h2>
+    <ul>
+        <li>555555555555555555555555555555555555555555555555555</li>
+        <li>555555555555555555555555555555555555555555555555555</li>
+        <li>555555555555555555555555555555555555555555555555555</li>
+    </ul>
+
+</body>
+
+</html>
